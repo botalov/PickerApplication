1e3bbb37e56a21487c9280b3f874374258b7514d
botalov <botalov.n@carrotquest.io>
2019-02-05 18:07:01 +0500

    Правки отступов холдеров в пикере

--- imagepicker/src/main/java/com/botalov/imagepicker/control/bottom_sheet/picker_bottom_sheet/adapter/PickerRecyclerViewAdapter.kt
+++ imagepicker/src/main/java/com/botalov/imagepicker/control/bottom_sheet/picker_bottom_sheet/adapter/PickerRecyclerViewAdapter.kt
@@ -1,7 +1,6 @@
 package com.botalov.imagepicker.control.bottom_sheet.picker_bottom_sheet.adapter
 
 import android.support.constraint.ConstraintLayout
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
